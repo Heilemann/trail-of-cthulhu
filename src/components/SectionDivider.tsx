35bc32c3b803f715b4c50487fde8af25b600d537
Michael Heilemann <michael.heilemann@me.com>
2022-08-29 21:59:39 -0400

    commit

--- src/components/SectionDivider.tsx
+++ src/components/SectionDivider.tsx
@@ -13,11 +13,12 @@ export default function SectionDivider({
 	return (
 		<div
 			className={twMerge(
-				`sectionDivider relative flex flex-none overflow-hidden text-xl font-bold uppercase tracking-wider text-gray-600 before:m-auto before:mr-4 before:h-0.5 before:flex-1 before:rounded-full before:bg-gray-800 before:content-[""] after:m-auto after:ml-4 after:h-0.5 after:flex-1 after:rounded-full after:bg-gray-800 after:content-[""]`,
+				`sectionDivider relative flex flex-none overflow-hidden text-lg uppercase tracking-wider text-gray-600 before:m-auto before:mr-4 before:h-0.5 before:flex-1 before:rounded-full before:bg-gray-800 before:content-[""] after:m-auto after:ml-4 after:h-0.5 after:flex-1 after:rounded-full after:bg-gray-800 after:content-[""]`,
 				className,
 			)}
 			style={{
 				fontFamily: 'CovingtonCondensed',
+				// fontVariant: 'small-caps',
 			}}
 		>
 			{children}
