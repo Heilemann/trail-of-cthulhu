568b36c8282404b1eb8681285440f7491752b22e
Michael Heilemann <michael.heilemann@me.com>
2022-12-10 21:34:25 -0500

    Updating

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useReducer } from 'react'
+import { useEffect, useReducer } from 'react'
 import './App.css'
 import Container from './components/Container'
 import Context from './components/context'
@@ -10,6 +10,11 @@ function App() {
 	// @ts-ignore
 	const [state, dispatch] = useReducer(Reducer, {} as TState)
 	const isDevelopment = process.env.NODE_ENV === 'development'
+
+	// console log if state is updated
+	useEffect(() => {
+		console.log('trail of cthulhu: state updated', state)
+	}, [state])
 
 	return (
 		<Context.Provider value={{ state, dispatch }}>
