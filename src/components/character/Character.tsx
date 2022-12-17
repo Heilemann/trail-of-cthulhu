84d2636726dc999678616722740f12468babb961
Michael Heilemann <michael.heilemann@me.com>
2022-12-16 22:25:09 -0500

    Updating

--- src/components/character/Character.tsx
+++ src/components/character/Character.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from 'react'
+import { useContext } from 'react'
 import { useWatch } from 'react-hook-form'
 import context from '../context'
 import DecoBox from '../DecoBox'
