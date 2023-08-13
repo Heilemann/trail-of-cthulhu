9a3687f54b60c3649c53dffa8e6090dfa1e29f2f
Michael Heilemann <michael.heilemann@me.com>
2023-08-12 21:18:51 -0400

    Fixing spend button in dark mode

--- src/components/character/SkillSpendButton.tsx
+++ src/components/character/SkillSpendButton.tsx
@@ -9,7 +9,7 @@ type Props = {
 export default function SkillSpendButton({ name, category }: Props) {
 	return (
 		<SkillPopover name={name} category={category}>
-			<div className='ml-1 flex aspect-square h-7 justify-center rounded-md bg-gray-200 text-gray-700'>
+			<div className='ml-1 flex aspect-square h-7 justify-center rounded-md bg-gray-200 text-gray-700 dark:bg-gray-800 darl:text-gray-200'>
 				<ChatBubbleLeftIcon
 					className='h-3.5 w-3.5 self-center'
 					aria-hidden='true'
