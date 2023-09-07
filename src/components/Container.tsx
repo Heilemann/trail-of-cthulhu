81e6ea95c6ff72fa414f5fe67df4f5b61ba16dae
Michael Heilemann <michael.heilemann@me.com>
2023-09-07 16:53:15 -0400

    Updating

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -99,19 +99,13 @@ export default function Container() {
 					const newDocument = data.documents?.find(
 						(d: TDocument) => d._id === state.documentId,
 					)
-					console.log(
-						'UPDATE DATA: newDocument',
-						newDocument,
-						data,
-						_.isEqual(newDocument?.values, state.document?.values),
-					)
 
 					if (!newDocument) {
 						console.error('New document not found')
 						return
 					}
 
-					if (_.isEqual(newDocument?.values, state.document?.values)) {
+					if (_.isEqual(newDocument, state.document)) {
 						return
 					}
 
