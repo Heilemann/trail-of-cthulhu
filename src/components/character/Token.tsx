83808c55a666a2b133831d32f465c472fbd6018b
Michael Heilemann <michael.heilemann@me.com>
2023-09-16 15:50:07 -0400

    Updating

--- src/components/character/Token.tsx
+++ src/components/character/Token.tsx
@@ -4,7 +4,11 @@ import DecoBox from '../DecoBox'
 export default function Token() {
 	return (
 		<DecoBox className='min-h-30 md:min-h-40 flex w-full flex-1 flex-col text-center md:w-full'>
-			<Asset name='token' addLabel='Add Token' removeLabel='Remove Token' />
+			<Asset
+				name='token'
+				addLabel='Add Portrait'
+				removeLabel='Remove Portrait'
+			/>
 		</DecoBox>
 	)
 }
