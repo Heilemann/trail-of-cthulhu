546e51c8bd176a6e9f9ab965f3872176384fdeb9
Michael Heilemann <michael.heilemann@me.com>
2023-09-17 21:45:27 -0400

    Updating

--- src/components/BaseComponents/hooks/UseMessageToApp.ts
+++ src/components/BaseComponents/hooks/UseMessageToApp.ts
@@ -1,11 +1,12 @@
 import { TAppReceivableMessages } from '../../../interfaces'
-import { useMemo } from 'react'
 
 export default function useMessageToApp() {
-	const documentId = useMemo(() => {
+	const documentId = () => {
+		// useMemo(() => {
 		const searchParams = new URLSearchParams(window.parent.location.search)
 		return searchParams.get('id')
-	}, [])
+	}
+	// }, [])
 
 	if (documentId === null) {
 		console.warn('Document ID is not specified in the parent URL.')
