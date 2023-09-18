dcf01ee0db77ccc2bcd2d002a52b407e01ee01f6
Michael Heilemann <michael.heilemann@me.com>
2023-09-17 21:28:42 -0400

    Updating

--- src/components/BaseComponents/hooks/UseMessageToApp.ts
+++ src/components/BaseComponents/hooks/UseMessageToApp.ts
@@ -12,7 +12,7 @@ export default function useMessageToApp() {
 			message,
 			data: {
 				...data,
-				documentId: state.document._id ?? null,
+				documentId: state?.document?._id ?? null,
 			},
 		} as TAppReceivableMessages)
 	}
