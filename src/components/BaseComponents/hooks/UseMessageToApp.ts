a2f6401577b56cf7f7d4538f04b88392eacecdc5
Michael Heilemann <michael.heilemann@me.com>
2023-09-17 21:46:21 -0400

    Updating

--- src/components/BaseComponents/hooks/UseMessageToApp.ts
+++ src/components/BaseComponents/hooks/UseMessageToApp.ts
@@ -1,26 +1,21 @@
 import { TAppReceivableMessages } from '../../../interfaces'
 
+// this is a hook that returns a function that sends a message to the parent window
 export default function useMessageToApp() {
-	const documentId = () => {
-		// useMemo(() => {
-		const searchParams = new URLSearchParams(window.parent.location.search)
-		return searchParams.get('id')
-	}
-	// }, [])
+	let searchParams = new URLSearchParams(window.parent.location.search)
+	let documentId = searchParams.get('id') // Replace 'myParam' with the actual parameter name
 
 	if (documentId === null) {
 		console.warn('Document ID is not specified in the parent URL.')
 	}
 
 	const messageToApp = ({ message, data }: TAppReceivableMessages) => {
-		if (documentId !== null) {
-			window.parent.postMessage({
-				source: 'System',
-				documentId,
-				message,
-				data,
-			} as TAppReceivableMessages)
-		}
+		window.parent.postMessage({
+			source: 'System',
+			message,
+			documentId,
+			data,
+		} as TAppReceivableMessages)
 	}
 
 	return messageToApp
