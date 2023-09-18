327256cdbd4d46b23fde423d44582c1e6e236644
Michael Heilemann <michael.heilemann@me.com>
2023-09-17 21:03:57 -0400

    Updating

--- src/components/BaseComponents/hooks/UseMessageToApp.ts
+++ src/components/BaseComponents/hooks/UseMessageToApp.ts
@@ -5,16 +5,9 @@ import context from '../context'
 // this is a hook that returns a function that sends a message to the parent window
 export default function useMessageToApp() {
 	const { state } = useContext(context)
-	const { document } = state || {}
-	const { _id } = document || {}
 
 	const messageToApp = ({ message, data }: TAppReceivableMessages) => {
-		if (!_id) {
-			console.error('Document or _id is undefined')
-			return
-		}
-
-		console.log('messageToApp', _id, message, data)
+		console.log('messageToApp', state?.document?._id, message, data)
 
 		window.parent.postMessage({
 			source: 'System',
