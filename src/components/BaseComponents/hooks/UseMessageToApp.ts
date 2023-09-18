d817d2f7ccd5ed9d9c9ae7c302560e4c7598ed93
Michael Heilemann <michael.heilemann@me.com>
2023-09-17 21:28:08 -0400

    Updating

--- src/components/BaseComponents/hooks/UseMessageToApp.ts
+++ src/components/BaseComponents/hooks/UseMessageToApp.ts
@@ -1,13 +1,18 @@
+import { useContext } from 'react'
 import { TAppReceivableMessages } from '../../../interfaces'
+import context from '../context'
 
 // this is a hook that returns a function that sends a message to the parent window
 export default function useMessageToApp() {
+	const { state } = useContext(context)
+
 	const messageToApp = ({ message, data }: TAppReceivableMessages) => {
 		window.parent.postMessage({
 			source: 'System',
 			message,
 			data: {
 				...data,
+				documentId: state.document._id,
 			},
 		} as TAppReceivableMessages)
 	}
