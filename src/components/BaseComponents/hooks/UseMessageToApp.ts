9f2d953434f44891353809d94569b338393d430c
Michael Heilemann <michael.heilemann@me.com>
2023-09-17 21:47:52 -0400

    Updating

--- src/components/BaseComponents/hooks/UseMessageToApp.ts
+++ src/components/BaseComponents/hooks/UseMessageToApp.ts
@@ -3,7 +3,8 @@ import { TAppReceivableMessages } from '../../../interfaces'
 // this is a hook that returns a function that sends a message to the parent window
 export default function useMessageToApp() {
 	let searchParams = new URLSearchParams(window.parent.location.search)
-	let documentId = searchParams.get('id') // Replace 'myParam' with the actual parameter name
+	let documentId = searchParams.get('id')
+	console.log('--------> documentId', documentId)
 
 	if (documentId === null) {
 		console.warn('Document ID is not specified in the parent URL.')
--- src/components/BaseComponents/hooks/usePostMessageListener.ts
+++ src/components/BaseComponents/hooks/usePostMessageListener.ts
@@ -30,8 +30,6 @@ const usePostMessageListener = ({ resetInProgress }: Props) => {
 
 			switch (message) {
 				case 'load':
-					console.log('System received load message:', data)
-
 					const { documentId } = data
 					const document = data.documents?.find(
 						(d: TDocument) => d._id === documentId,
