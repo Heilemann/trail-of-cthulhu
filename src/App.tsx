5dce95653cc93a99a9501a8c43a75d1b0c911ca7
Michael Heilemann <michael.heilemann@me.com>
2024-08-02 15:21:27 -0400

    Updating

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { useReducer } from 'react'
 import { FormProvider, useForm } from 'react-hook-form'
+import './App.css'
+import './assets/logo.png'
 import Container from './components/BaseComponents/Container'
 import DragAndDrop from './components/BaseComponents/DragAndDrop'
 import Context from './components/BaseComponents/context'
 import DevToolbar from './components/BaseComponents/dev/DevMode'
 import Reducer from './components/BaseComponents/reducer'
 import { TState, TValues } from './interfaces'
-import './App.css'
 
 function App() {
 	const [state, dispatch] = useReducer(Reducer, {} as TState)
