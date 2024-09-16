9f5fe067eebffd8b7ba2c012fb4f56fdf73c1a09
Michael Heilemann <michael.heilemann@me.com>
2024-09-15 20:15:56 -0400

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -3,37 +3,48 @@ const replace = require('gulp-replace')
 const inlinesource = require('gulp-inline-source')
 
 const cleanUp = () => {
-	return src('./dist/*.html')
+	return src('./build/*.html')
 		.pipe(replace('<link rel="manifest" href="/manifest.json"/>', ''))
 		.pipe(replace('<link rel="icon" href="/favicon.ico"/>', ''))
 		.pipe(replace('<link rel="apple-touch-icon" href="/logo192.png"/>', ''))
-		.pipe(dest('./dist'))
+		.pipe(dest('./build'))
 }
 
 const inlineScriptsAndCSS = () => {
-	const regex = /<script.*?<\/script>/gs
-	return src('./dist/*.html')
-		.pipe(replace(regex, ''))
-		.pipe(
-			replace('</body>', match => {
-				return `<script src="./assets/index-Bsz-2xDw.js"></script>${match}`
-			}),
-		)
-		.pipe(replace('.js"></script>', '.js" inline></script>'))
-		.pipe(replace('rel="stylesheet">', 'rel="stylesheet" inline>'))
-		.pipe(
-			inlinesource({
-				compress: false,
-				rootpath: './dist',
-			}),
-		)
-		.pipe(dest('./dist'))
+	const regex = /<script.*<\/script>/g
+	return (
+		src('./build/*.html')
+			// move <script> to end of file, as defer won't work with inline scripts
+			// TODO: This is broken, it duplicates code, but it sort of works for now
+			.pipe(
+				replace(regex, function replace(match, offset, string) {
+					const newString = string
+						.replace(match, '')
+						.replace('</body></html>', match + '</body></html>')
+					return newString
+				}),
+			)
+			// inline js and css
+			.pipe(replace('.js"></script>', '.js" inline></script>'))
+			.pipe(replace('rel="stylesheet">', 'rel="stylesheet" inline>'))
+			.pipe(
+				inlinesource({
+					compress: false,
+				}),
+			)
+			.pipe(dest('./dist'))
+	)
 }
 
+// rename path to asset files (which are uploaded on installation)
 const renameAssetsPaths = () => {
 	return src('./dist/index.html')
-		.pipe(replace('assets/', 'files/'))
+		.pipe(replace('static/media/', 'files/'))
 		.pipe(dest('./dist'))
 }
 
-export default series(cleanUp, inlineScriptsAndCSS, renameAssetsPaths)
+// const watchTask = () => {
+// 	watch('build/*.[html, js, css]', inlineScripts)
+// }
+
+exports.default = series(cleanUp, inlineScriptsAndCSS, renameAssetsPaths)
